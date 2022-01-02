bba26974c251c262365acc832fa628d5aa8c4380
karthick srinivasan <karthicksrinivasan89@gmail.com>
2022-01-02 23:55:19 +0530

    js fix

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ links.forEach((link,i) =>{
 //     body: new URLSearchParams(formData).toString()
 //   }).then(() => console.log('Form successfully submitted')).catch((error) =>
 //     alert(error))
-}//
+//
 
 
