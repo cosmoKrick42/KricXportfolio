122bb88e0e1589185c882b27a1b0a31e932ff22b
karthick srinivasan <karthicksrinivasan89@gmail.com>
2022-02-15 23:08:50 +0530

    Added Resume and Changed about timeline

--- app.js
+++ app.js
@@ -1,30 +1,58 @@
-const links = document.querySelectorAll('.link');
-const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll(".link");
+const sections = document.querySelectorAll("section");
 
-let activeLink=0;
-links.forEach((link,i) =>{
-    link.addEventListener('click',() =>{
-        if(activeLink !=i){
-            links[activeLink].classList.remove('active');
-            link.classList.add('active');
-            sections[activeLink].classList.remove('active');
+let activeLink = 0;
+links.forEach((link, i) => {
+  link.addEventListener("click", () => {
+    if (activeLink != i) {
+      links[activeLink].classList.remove("active");
+      link.classList.add("active");
+      sections[activeLink].classList.remove("active");
 
-            setTimeout(() => {
-                activeLink = i;
-                sections[i].classList.add('active');
-            },100);
+      setTimeout(() => {
+        activeLink = i;
+        sections[i].classList.add("active");
+      }, 100);
+    }
+  });
+});
 
+function DownloadFile(fileName) {
+  //Set the File URL.
+  var url = "Files/" + fileName;
 
-        }
-    })
-})
+  //Create XMLHTTP Request.
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.responseType = "blob";
+  req.onload = function () {
+    //Convert the Byte Data to BLOB object.
+    var blob = new Blob([req.response], { type: "application/octetstream" });
+
+    //Check the Browser type and download the File.
+    var isIE = false || !!document.documentMode;
+    if (isIE) {
+      window.navigator.msSaveBlob(blob, fileName);
+    } else {
+      var url = window.URL || window.webkitURL;
+      link = url.createObjectURL(blob);
+      var a = document.createElement("a");
+      a.setAttribute("download", fileName);
+      a.setAttribute("href", link);
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+  req.send();
+};
 
 debugger;
 function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
-    audio.volume = 0.1;
-  }
+  var audio = document.getElementById("audio");
+  audio.play();
+  audio.volume = 0.1;
+}
 
 //form submit
 // const name_form=document.getElementById('name');
@@ -58,5 +86,3 @@ function play() {
 //   }).then(() => console.log('Form successfully submitted')).catch((error) =>
 //     alert(error))
 //
-
-
